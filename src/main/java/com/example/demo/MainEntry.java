a573ee82c5fa239512b4ac44e04175205d60484c
Lin Yifeng <dilsiyu@163.com>
2021-10-01 16:08:04 +0800

    "day 1"

---
+++ src/main/java/com/example/demo/MainEntry.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+/**
+ * @ClassName: MainEntry
+ * @Description: for git submmit
+ * @Author: Lin Yifeng
+ * @Date: 2021-10-01 16:06
+ * @Version: 1.0
+ **/
+public class MainEntry {
+}
